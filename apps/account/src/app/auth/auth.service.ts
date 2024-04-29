53634c719f62789031a81552f55f1b2ca4089f56
anton.liubushko <anton.liubushko@amdaris.com>
2024-04-29 17:59:20 +0300

    Implemented authenticatoin

--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../user/models/user.model';
+import { RegisterDto } from './auth.controller';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserRole } from '@jiraf/interfaces';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+  ){}
+
+  async register({email,password, displayName}: RegisterDto): Promise<{email: string}> {
+    const oldUser = await this.userRepository.findUser(email);
+    if(oldUser){
+      throw new Error("This user is already registered");
+    }
+    const newUserEntity = await new UserEntity({
+      email,
+      displayName,
+      passwordHash: '',
+      role: UserRole.Student
+    }).setPassword(password)
+    const newUser = await this.userRepository.createUser(newUserEntity);
+
+    return {email: newUser.email};
+  }
+
+  async validateUser(email: string, password: string){
+    const user = await this.userRepository.findUser(email);
+    if(!user){
+      throw new Error("Incorrect email or password");
+    }
+    const userEntity = await new UserEntity(user);
+    const isCorrectPassword = await userEntity.validatePassword(password);
+    if(!isCorrectPassword){
+      throw new Error("Incorrect email or password");
+    }
+
+    return {id: user._id}
+  }
+
+  async login(id: string){
+    return {
+      access_token: await this.jwtService.signAsync({id})
+    }
+  }
+}
--- apps/account/src/app/configs/mongo.config.ts
+++ apps/account/src/app/configs/mongo.config.ts
@@ -4,11 +4,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 export const getMongoConfig = (): MongooseModuleAsyncOptions => {
   return {
     useFactory: (configService: ConfigService) => ({
-      url: getMongoString(configService)
+      uri: getMongoString(configService)
     }),
-    inject: ConfigService,
-    imports: ConfigModule
+    inject: [ConfigService],
+    imports: [ConfigModule]
   }
 }
 
-const getMongoString = (configService: ConfigService) => `'mongodb://'${configService.get('MONGO_LOGIN')}:${configService.get('MONGO_PASSWORD')}@${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DATABASE')}?authSource=${configService.get('MONGO_AUTHDATABASE')}`
+const getMongoString = (configService: ConfigService) => `mongodb://${configService.get('MONGO_LOGIN')}:${configService.get('MONGO_PASSWORD')}@${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DATABASE')}?authSource=${configService.get('MONGO_AUTHDATABASE')}`
